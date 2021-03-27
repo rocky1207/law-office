037cdc2f6d759eb236b438669bb1514bcc47c22f
rocky1207 <mjrocky1918@gmail.com>
2021-03-27 15:53:16 +0100

    First about.php commit

---
+++ about.php
@@ -0,0 +1,52 @@
+<?php include 'partials/main-header.php';
+?>
+<?php include 'partials/peoples.php';
+?>  
+    <section class="types">
+        <h3>Types of personal unjury and accident cade we handle</h3>
+        <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, impedit dignissimos maiores eveniet consectetur voluptate corporis laborum quod. Sed laborum, dignissimos nobis dolore magnam cupiditate?
+        </span></p>
+        <div class="container">
+            <article>
+                <h4>Personal injury</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus libero, quia recusandae blanditiis ipsam, repellat esse molestiae aut ratione laboriosam voluptates corporis tempore dignissimos tempora laborum iste eligendi praesentium minus odio voluptatem? Fugiat, aut cupiditate.</p>
+            </article>
+            <article>
+                <h4>Wrongfull death</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus libero, quia recusandae blanditiis ipsam, repellat esse molestiae aut ratione laboriosam voluptates corporis tempore dignissimos tempora laborum iste eligendi praesentium minus odio voluptatem? Fugiat, aut cupiditate.</p>
+            </article>
+            <article>
+                <h4>Medical malpractice</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus libero, quia recusandae blanditiis ipsam, repellat esse molestiae aut ratione laboriosam voluptates corporis tempore dignissimos tempora laborum iste eligendi praesentium minus odio voluptatem? Fugiat, aut cupiditate.</p>
+            </article>
+            <article>
+                <h4>Promisess liability</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus libero, quia recusandae blanditiis ipsam, repellat esse molestiae aut ratione laboriosam voluptates corporis tempore dignissimos tempora laborum iste eligendi praesentium minus odio voluptatem? Fugiat, aut cupiditate.</p>
+            </article>
+            <article>
+                <h4>Auto accidents</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus libero, quia recusandae blanditiis ipsam, repellat esse molestiae aut ratione laboriosam voluptates corporis tempore dignissimos tempora laborum iste eligendi praesentium minus odio voluptatem? Fugiat, aut cupiditate.</p>
+            </article>
+            <article>
+                <h4>Track accidents</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus libero, quia recusandae blanditiis ipsam, repellat esse molestiae aut ratione laboriosam voluptates corporis tempore dignissimos tempora laborum iste eligendi praesentium minus odio voluptatem? Fugiat, aut cupiditate.</p>
+            </article>
+            <article>
+                <h4>Unisured motorists</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus libero, quia recusandae blanditiis ipsam, repellat esse molestiae aut ratione laboriosam voluptates corporis tempore dignissimos tempora laborum iste eligendi praesentium minus odio voluptatem? Fugiat, aut cupiditate.</p>
+            </article>
+            <article>
+                <h4>Motorcycle accidents</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus libero, quia recusandae blanditiis ipsam, repellat esse molestiae aut ratione laboriosam voluptates corporis tempore dignissimos tempora laborum iste eligendi praesentium minus odio voluptatem? Fugiat, aut cupiditate.</p>
+            </article>
+        </div>
+    </section>
+<?php include 'partials/footer.php';
+?>
+
+
+
+
+
+
+ 
