f3e5671281baa231d7e8cf5bd8e51073b6653138
rocky1207 <mjrocky1918@gmail.com>
2021-03-27 16:06:13 +0100

    Firs partials commit

---
+++ partials/footer.php
@@ -0,0 +1,32 @@
+   <!-- contact -->
+   <section class="contact container">
+        <article>
+            <h3>Address</h3>
+            <p>Calgary, Alberta</p>
+            <p>CANADA</p>
+        </article>
+        
+        <article>
+            <h3>Business Hours</h3>
+            <p>MON / FRI - 10:00/18:00</p>
+            <p>SAT - 11:00/15:00</p>
+        </article>
+        <article>
+            <h3>Contact</h3>
+            <p>Phone: 0234 567</p>
+            <p>Email: email@lawoffice.com</p>
+        </article>
+    </section>
+    <!-- map -->
+    <section class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d321202.8134694146!2d-114.3680122994638!3d51.027288334012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2z0JrQsNC70LPQsNGA0LgsINCQ0LvQsdC10YDRgtCwLCDQmtCw0L3QsNC00LA!5e0!3m2!1ssr!2srs!4v1615840284473!5m2!1ssr!2srs" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </section>
+        <!-- footer -->
+        <footer>
+            <p>&copy; 2021. webcon.rs</p>
+        </footer>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
---
+++ partials/peoples.php
@@ -0,0 +1,41 @@
+    <!-- peoples -->
+    <section class="peoples">
+            <div class="container">
+                <article>
+                    <img src="img/people1.jpg" alt="">
+                    <div class="text">
+                        <h3>Etta James</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quod a natus quae repudiandae? Odio, blanditiis molestiae. Ipsum natus minima nemo ab, corporis, laborum, ipsa mollitia ut reprehenderit dignissimos modi.</p>
+                        <button>learn more</button>
+                        <button>contact</button>
+                    </div>
+                </article>
+                <article>
+                    <img src="img/people2.jpg" alt="">
+                    <div class="text">
+                        <h3>Louis Armstrong</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quod a natus quae repudiandae? Odio, blanditiis molestiae. Ipsum natus minima nemo ab, corporis, laborum, ipsa mollitia ut reprehenderit dignissimos modi.</p>
+                        <button>learn more</button>
+                        <button>contact</button>
+                    </div>
+                </article>
+                <article>
+                    <img src="img/people3.jpg" alt="">
+                    <div class="text">
+                        <h3>Bessie Smith</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quod a natus quae repudiandae? Odio, blanditiis molestiae. Ipsum natus minima nemo ab, corporis, laborum, ipsa mollitia ut reprehenderit dignissimos modi.</p>
+                        <button>learn more</button>
+                        <button>contact</button>
+                    </div>
+                </article>
+                <article>
+                    <img src="img/people4.jpg" alt="">
+                    <div class="text">
+                        <h3>Elmore James</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quod a natus quae repudiandae? Odio, blanditiis molestiae. Ipsum natus minima nemo ab, corporis, laborum, ipsa mollitia ut reprehenderit dignissimos modi.</p>
+                        <button>learn more</button>
+                        <button>contact</button>
+                    </div>
+                </article>
+            </div>
+        </section>
