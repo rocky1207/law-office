ccbc3252a6f858561f6fb8c1d3bceb02b83e75b4
rocky1207 <mjrocky1918@gmail.com>
2021-03-27 15:52:01 +0100

    First index.php commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php include 'partials/main-header.php';
+?>
+    <!-- services -->
+    <section class="services container">
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Family low</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+            <button>Learn more</button>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Business law</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+            <button>Learn more</button>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Criminal law</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+            <button>Learn more</button>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Insurance law</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+            <button>Learn more</button>
+        </article>
+    </section>
+
+    <!-- about -->
+    <section class="about">
+        <article>
+            <h2>About us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam vel cum nulla mollitia earum ad maxime laudantium ipsa cupiditate totam nostrum vero, reiciendis consequatur sunt voluptates laborum, ducimus repellendus possimus at autem deleniti! Facilis est vero cum consequuntur aliquam nobis!</p>
+            <a href="">Learn more &nbsp; &rang;</a>
+        </article>
+        <article>
+            <h2>Welcome to <br> <span>Solo Practice</span> <br> Law</h2>
+        </article>
+    </section>
+
+    <!-- team -->
+    <section class="team">
+        <h2>Our team</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime animi eaque sed esse eum eius consectetur neque praesentium voluptatum? Cupiditate, ex, ratione. Delectus fuga in iste assumenda ipsum perspiciatis? Magni adipisci pariatur eveniet explicabo velit dolores sunt aspernatur ipsam dolorum nihil facere veritatis, aperiam delectus id rem vitae fugiat iste laboriosam similique amet, ratione quasi. Tempora magni soluta temporibus pariatur voluptatibus natus iusto, repellat ratione qui, assumenda, recusandae quos id! Ea impedit, culpa optio aperiam, beatae quibusdam ex deleniti minima.</p>
+    </section>
+    <!-- peoples -->
+    <?php include 'partials/peoples.php';
+    ?>  
+    <!--consultation-->
+    <section class="consultation">
+        <article>
+            <h2>Get a free no <br> obligation <br> consultation</h2>
+        </article>
+        <article>
+            <form>
+                <input type="text"placeholder="First name">
+                <input type="text"placeholder="Last name">
+                <input type="email"placeholder="Email">
+                <textarea cols="30" rows="10" placeholder="Message"></textarea>
+                <button>Submit</button>
+            </form>
+        </article>
+    </section>
+
+    <!-- testimonials -->
+    <section class="testimonials">
+        <h2>Testimonials</h2>
+        <article class="owl-carousel owl-theme">
+            <div class="item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum suscipit ipsa inventore error magni quae impedit quo sapiente minus nihil tempora accusamus cum, quaerat adipisci! Praesentium, ipsa! Vel, repellat reiciendis.</p>
+                <h3>Client name 1</h3>
+            </div>
+            <div class="item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum suscipit ipsa inventore error magni quae impedit quo sapiente minus nihil tempora accusamus cum, quaerat adipisci! Praesentium, ipsa! Vel, repellat reiciendis.</p>
+                <h3>Client name 2</h3>
+            </div>
+            <div class="item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum suscipit ipsa inventore error magni quae impedit quo sapiente minus nihil tempora accusamus cum, quaerat adipisci! Praesentium, ipsa! Vel, repellat reiciendis.</p>
+                <h3>Client name 3</h3>
+            </div>
+        </article>
+    </section>
+
+
+<?php include 'partials/footer.php';
+?>
+
+
+
+
+
+
+ 
