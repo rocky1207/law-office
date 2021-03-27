c3a57093062e32c17cabae79addf2ec8e76cd305
rocky1207 <mjrocky1918@gmail.com>
2021-03-27 15:55:10 +0100

    First blog.php commit

---
+++ blog.php
@@ -0,0 +1,94 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Law office</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="scss/style.css">
+</head>
+<body>
+    <!-- top bar -->
+    <section class="topBar container">
+        <a href="index.php" class="logo">Law <span>Office</span></a>
+        <ul>
+            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+            <li><a href=""><i class="fab fa-instagram"></i></a></li>
+        </ul>
+    </section>
+    <!-- mainHeader -->
+    <header class="headHead mainHeader">
+        <nav class="container">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <div>
+            <h2>BLOG</h2>
+        </div>
+    </header>
+    <main class="container">
+        <section>
+            <article>
+                <figure>
+                    <img src="img/about2.jpg" alt="">
+                </figure>
+                <div>
+                    <h4>Blog1</h4>
+                    <p>Admin &Iota; 20.12.2020.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nemo ratione unde eligendi fuga consectetur, assumenda tenetur cum odit vero aspernatur, sed ipsa aperiam ea officia sunt tempore voluptas ipsum neque iure nam, dicta autem fugit laboriosam. Laboriosam beatae itaque veritatis atque necessitatibus laborum molestias, consequuntur optio eligendi dolore veniam.</p>
+                    <a href="">Read more &rsaquo;&rsaquo;</a>
+                </div>
+            </article>
+            <article>
+            <figure>
+                <img src="img/about.jpg" alt="">
+                </figure>
+                <div>
+                    <h4>Blog2</h4>
+                    <p>Admin &Iota; 20.12.2020.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nemo ratione unde eligendi fuga consectetur, assumenda tenetur cum odit vero aspernatur, sed ipsa aperiam ea officia sunt tempore voluptas ipsum neque iure nam, dicta autem fugit laboriosam. Laboriosam beatae itaque veritatis atque necessitatibus laborum molestias, consequuntur optio eligendi dolore veniam.</p>
+                    <a href="">Read more &rsaquo;&rsaquo;</a>
+                </div>
+            </article>
+            <article>
+            <figure>
+                <img src="img/team.jpg" alt="">
+                </figure>
+                <div>
+                    <h4>Blog3</h4>
+                    <p>Admin &Iota; 20.12.2020.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nemo ratione unde eligendi fuga consectetur, assumenda tenetur cum odit vero aspernatur, sed ipsa aperiam ea officia sunt tempore voluptas ipsum neque iure nam, dicta autem fugit laboriosam. Laboriosam beatae itaque veritatis atque necessitatibus laborum molestias, consequuntur optio eligendi dolore veniam.</p>
+                    <a href="">Read more &rsaquo;&rsaquo;</a>
+                </div>
+            </article>
+            <article>
+            <figure>
+                <img src="img/header.jpg" alt="">
+                </figure>
+                <div>
+                    <h4>Blog4</h4>
+                    <p>Admin &Iota; 20.12.2020.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nemo ratione unde eligendi fuga consectetur, assumenda tenetur cum odit vero aspernatur, sed ipsa aperiam ea officia sunt tempore voluptas ipsum neque iure nam, dicta autem fugit laboriosam. Laboriosam beatae itaque veritatis atque necessitatibus laborum molestias, consequuntur optio eligendi dolore veniam.</p>
+                    <a href="">Read more &rsaquo;&rsaquo;</a>
+                </div>
+            </article>
+        </section>
+        <aside>
+            <h4>Categories</h4>
+            <a href="">Category1</a>
+            <a href="">Category2</a> 
+            <a href="">Category3</a> 
+            <a href="">Category4</a> 
+        </aside>
+    </main>
+<?php
+include "partials/footer.php";
+?>
